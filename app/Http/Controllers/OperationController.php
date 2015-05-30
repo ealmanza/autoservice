f652a9753e1206b81cbed6a9a15cf584eb873b6d
Edgar Almanza <edgaralmanza@gmail.com>
2015-05-29 19:30:37 -0500

    Laravel

---
+++ app/Http/Controllers/OperationController.php
@@ -0,0 +1,104 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class OperationController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+    public function sum()
+    {
+        return "Se Esta Ejecutando La Suma";
+    }
+
+    public function subtraction()
+    {
+        return "Se Esta Ejecutando La Resta";
+    }
+
+    public function multiplication()
+    {
+        return "Se Esta Ejecutando La Multiplicación";
+    }
+
+    public function divicion()
+    {
+        return "Se Esta Ejecutando La Divición";
+    }
+
+}
