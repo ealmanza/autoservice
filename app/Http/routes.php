d6e8e1020a9d4c7720705e65e338842e565f376f
Edgar Almanza <edgaralmanza@gmail.com>
2015-05-29 19:26:31 -0500

    Laravel

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,17 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+/**
+ * This Route Sum Many Numbers
+ */
+Route::get('/sum','OperationController@sum');
+
+Route::get('/subtraction','OperationController@subtraction');
+
+Route::get('/multiplication','OperationController@multiplication');
+
+Route::get('/divicion','OperationController@divicion');
+
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
